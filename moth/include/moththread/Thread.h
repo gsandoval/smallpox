a31fd605315e66e7579eabf37e28ff46cfdd2fc4
Guillermo Sandoval <gsandoval@darchitect.org>
2011-06-24 09:27:44 -0500

    Adds .gitignore sets makefile up

---
+++ moth/include/moththread/Thread.h
@@ -0,0 +1,20 @@
+/*
+ * Thread.h
+ *
+ *  Created on: 23/06/2011
+ *      Author: darchitect
+ */
+
+#ifndef THREAD_H_
+#define THREAD_H_
+
+namespace moth {
+
+class Thread {
+public:
+	Thread();
+	virtual ~Thread();
+};
+
+} /* namespace moth */
+#endif /* THREAD_H_ */
