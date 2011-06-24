a2030af7e568b0f7ee4da2eaa89c22bc15342d97
Guillermo Sandoval <gsandoval@darchitect.org>
2011-06-23 23:22:08 -0500

    First commit

---
+++ moth/src/smallpox.cpp
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : smallpox.cpp
+ Author      : Guillermo Sandoval [gsandoval@darchitect.org]
+ Version     :
+ Copyright   : 
+ Description : Hello World in C++,
+ ============================================================================
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	cout << "Hello World!!!" << endl; /* prints Hello World!!! */
+	return 0;
+}
